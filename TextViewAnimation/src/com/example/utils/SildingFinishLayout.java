c3293aada7ae87023fff91d9b42f0992a7ee05a8
5354xyz <xiaoyizong@126.com>
2014-10-08 11:42:03 +0800

    fix sildingfinishLayout.java

--- TextViewAnimation/src/com/example/utils/SildingFinishLayout.java
+++ TextViewAnimation/src/com/example/utils/SildingFinishLayout.java
@@ -16,7 +16,6 @@ import android.widget.Scroller;
  * 自定义可以滑动的RelativeLayout, 类似于IOS的滑动删除页面效果，当我们要使用
  * 此功能的时候，需要将该Activity的顶层布局设置为SildingFinishLayout
  * 要注意：android 触摸事件分发拦截（如果子View或者子ViewGroup里面还有事件监听方法要先排除）
- * @author 5354xyz
  * 
  * @email  xiaoyizong@126.com
  * 
@@ -55,7 +54,19 @@ public class SildingFinishLayout extends RelativeLayout{
 	
 	private OnSildingFinishListener onSildingFinishListener;
 	private boolean isFinish;
+	private int direction = -1;
+	/**
+	 * 是否可以向某个方向滚动
+	 */
+	private boolean isSetLeft = false;
+	private boolean isSetRight = false;
+	private boolean isSetLeft_Right = false;
+	/**
+	 * 是否是人工滚动回原来的地方
+	 */
+	private boolean byPeople = false;
 	
+
 	public SildingFinishLayout(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
@@ -66,7 +77,22 @@ public class SildingFinishLayout extends RelativeLayout{
 		mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
 		mScroller = new Scroller(context);
 	}
-	
+	/**
+	 * 1 代表向左滑动
+	 * 2代表向右
+	 * 3代表左右
+	 * @param direction
+	 */
+	public void setSlidingDirection(int direction)
+	{
+		switch(direction){
+			case 1:isSetLeft=true;break;
+			case 2:isSetRight=true;break;
+			case 3:isSetLeft_Right=true;break;
+			default:return;
+		}
+			
+	}
 	/**
 	 * 事件拦截操作
 	 */
@@ -83,7 +109,8 @@ public class SildingFinishLayout extends RelativeLayout{
 			//满足此条件屏蔽SildingFinishLayout里面子类的touch事件
 			if (Math.abs(moveX - downX) > mTouchSlop
 					&& Math.abs((int) ev.getRawY() - downY) < mTouchSlop 
-					&& !checkIndisbootchoose(ev)) {
+					&& !checkIndisbootchoose(ev) && (isSetLeft||isSetRight||isSetLeft_Right)) {
+				System.out.println("屏蔽子类的touch事件");
 				return true;
 			}
 			break;
@@ -95,37 +122,47 @@ public class SildingFinishLayout extends RelativeLayout{
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		if(!checkIndisbootchoose(event)){
-		switch (event.getAction()) {
-		case MotionEvent.ACTION_MOVE:
-			int moveX = (int) event.getRawX();
-			int deltaX = tempX - moveX;
-			tempX = moveX;
-			if (Math.abs(moveX - downX) > mTouchSlop
-					&& Math.abs((int) event.getRawY() - downY) < mTouchSlop) {
-				isSilding = true;
+		if (!checkIndisbootchoose(event)) {
+			switch (event.getAction()) {
+			case MotionEvent.ACTION_MOVE:
+				int moveX = (int) event.getRawX();
+				int deltaX = tempX - moveX;
+				tempX = moveX;
+				if (Math.abs(moveX - downX) > mTouchSlop
+						&& Math.abs((int) event.getRawY() - downY) < mTouchSlop) {
+					isSilding = true;
+				}
+
+				if (Math.abs(moveX - downX) >= 0 && isSilding) {
+					if (((moveX - downX) > 0 && isSetLeft)
+							|| ((moveX - downX) < 0 && isSetRight)
+							|| isSetLeft_Right)
+						mParentView.scrollBy(deltaX, 0);
+				}
+				break;
+			case MotionEvent.ACTION_UP:
+				isSilding = false;
+				if (mParentView.getScrollX() <= -viewWidth / 2) {
+					isFinish = true;
+					direction = 1;
+					scrollRight();
+				} else if (mParentView.getScrollX() >= viewWidth / 2) {
+					isFinish = true;
+					direction = 2;
+					scrollLeft();
+				} else {
+					isFinish = false;
+					direction = 0;
+					scrollOrigin(false);
+
+				}
+				break;
 			}
-			
-			if (moveX - downX >= 0 && isSilding) {
-				mParentView.scrollBy(deltaX, 0);
-			}
-			break;
-		case MotionEvent.ACTION_UP:
-			isSilding = false;
-			if (mParentView.getScrollX() <= -viewWidth / 2) {
-				isFinish = true;
-				scrollRight();
-			} else {
-				scrollOrigin();
-				isFinish = false;
-			}
-			break;
-		}
 		}
 		return true;
 	}
 	
-	
+
 	@Override
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
 		super.onLayout(changed, l, t, r, b);
@@ -148,7 +185,7 @@ public class SildingFinishLayout extends RelativeLayout{
 
 
 	/**
-	 * 滚动出界面
+	 * 从左向右滚动出界面
 	 */
 	private void scrollRight() {
 		final int delta = (viewWidth + mParentView.getScrollX());
@@ -157,11 +194,24 @@ public class SildingFinishLayout extends RelativeLayout{
 				Math.abs(delta));
 		postInvalidate();
 	}
+	
+	/**
+	 * 从左向右滚动出界面
+	 */
+	private void scrollLeft() {
+		final int delta = (viewWidth - mParentView.getScrollX());
+		// 调用startScroll方法来设置一些滚动的参数，我们在computeScroll()方法中调用scrollTo来滚动item
+		mScroller.startScroll(mParentView.getScrollX(), 0, delta - 1, 0,
+				Math.abs(delta));
+		postInvalidate();
+	}
 
 	/**
 	 * 滚动到起始位置
 	 */
-	private void scrollOrigin() {
+	public void scrollOrigin(boolean byPeople) {
+		//getScrollX() ---> Return the scrolled left position of this view(返回偏离了屏幕左边界多远  “正数(左边界|)负数”)
+		this.byPeople = byPeople;
 		int delta = mParentView.getScrollX();
 		mScroller.startScroll(mParentView.getScrollX(), 0, -delta, 0,
 				Math.abs(delta));
@@ -171,7 +221,7 @@ public class SildingFinishLayout extends RelativeLayout{
 
 	@Override
 	public void computeScroll() {
-		// 调用startScroll的时候scroller.computeScrollOffset()返回true，
+		// 调用startScroll的时候scroller.computeScrollOffset()返回值为boolean，true说明滚动尚未完成，false说明滚动已经完成
 		if (mScroller.computeScrollOffset()) {
 			mParentView.scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
 			postInvalidate();
@@ -179,10 +229,14 @@ public class SildingFinishLayout extends RelativeLayout{
 			if (mScroller.isFinished() && isFinish) {
 
 				if (onSildingFinishListener != null) {
-					onSildingFinishListener.onSildingFinish();
+					if(!this.byPeople){
+						onSildingFinishListener.onSildingFinish(direction);
+						}
+					this.byPeople = false;
+						
 				}else{
 					//没有设置OnSildingFinishListener，让其滚动到其实位置
-					scrollOrigin();
+					scrollOrigin(false);
 					isFinish = false;
 				}
 			}
@@ -191,10 +245,16 @@ public class SildingFinishLayout extends RelativeLayout{
 	
 
 	public interface OnSildingFinishListener {
-		public void onSildingFinish();
+		/**
+		 * 从左向右滚动出界面 type=1
+		 * 从右向左滚动出界面 type=2
+		 * @param type
+		 */
+		public void onSildingFinish(int type);
 	}
+
 	/**
-	 * 判断手势是否落在一些不需要执行向右滑动销毁的区域内内
+	 * 判断手势是否落在一些不需要执行向右滑动销毁的区域内内，这个地方写死了，可以修改
 	 * 2014-4-13
 	 * 
 	 * @author:5354xyz
@@ -219,5 +279,4 @@ public class SildingFinishLayout extends RelativeLayout{
 		}
 			return false;
 	}
-
 }
